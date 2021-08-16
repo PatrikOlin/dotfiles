402a561c92cd2440bc6d62a546714441d67599a2
Patrik Olin <enmejl@gmail.com>
2021-08-16 07:38:10 +0200

    updated tmux.conf

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -1,1 +1,15 @@
 (put 'upcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(wakatime-api-key "954b96a3-3364-4821-bca2-eccfe5d1fa27")
+ '(warning-suppress-log-types '((lsp-mode) (lsp-mode)))
+ '(warning-suppress-types '((lsp-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
